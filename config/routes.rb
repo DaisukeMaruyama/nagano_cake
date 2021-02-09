85e69c77a1dce4149dfea50888c31ef7aff23d5a
DaisukeMaruyma <maruyama.d8@gmail.com>
2021-02-09 00:42:23 +0000

    [update]商品一覧レイアウト作成

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,21 @@ Rails.application.routes.draw do
 
   root to: 'homes#top'
   get '/about' => 'homes#about'
-  
 
   devise_for :customers
+
+  resources :items, only: [:show, :index]
+
+
+
+  namespace :admin do
+    get '/', to:'homes#top'
+  end
+
+  namespace :admin do
+    resources :items
+  end
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
