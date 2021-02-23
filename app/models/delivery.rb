946b19ce3a1f82abcc297e80e82e5ff28ecc7592
DaisukeMaruyama <maruyama.d8@gmail.com>
2021-02-23 14:01:35 +0000

    [Update]注文送付先内容編集

--- app/models/delivery.rb
+++ app/models/delivery.rb
@@ -1,8 +1,13 @@
 class Delivery < ApplicationRecord
-  
+
   validates :postal_code, presence: true
 	validates :address, presence: true
 	validates :name, presence: true
-  
+
   belongs_to :customer
+
+  def full_shipping_address
+    "〒"+self.postal_code + "　" + self.address + "　" + self.name
+  end
+
 end
