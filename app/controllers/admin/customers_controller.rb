d3f8f0ea1546799b79350b0b894b02850e050059
DaisukeMaruyama <maruyama.d8@gmail.com>
2021-02-24 01:09:42 +0000

    [Update]コントローラー編集

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -5,11 +5,38 @@ class Admin::CustomersController < ApplicationController
   end
 
   def show
+    @customer = Customer.find(params[:id])
   end
 
   def update
+    if customer.update(admin_customer_params)
+      flash[:notice] = "会員の編集を保存しました。"
+      redirect_to admin_customers_path
+    else
+      render :edit
+    end
   end
 
   def edit
+    @customer = Customer.find(params[:id])
   end
+  
+  private
+  
+  def admin_customer_params
+    params.require(:customer).permit(
+      :last_name, 
+      :first_name, 
+      :last_name_kana,
+      :first_name_kana, 
+      :phone_number, 
+      :postal_code, 
+      :address, 
+      :email, 
+      :is_deleted
+      )
+  end
+  
+  
+  
 end
