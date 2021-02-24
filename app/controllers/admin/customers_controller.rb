1c23b0d213eefeb275c90397b355ff722217e8c8
DaisukeMaruyama <maruyama.d8@gmail.com>
2021-02-24 00:56:57 +0000

    [Add]管理者画面会員管理画面追加

---
+++ app/controllers/admin/customers_controller.rb
@@ -0,0 +1,15 @@
+class Admin::CustomersController < ApplicationController
+
+  def index
+    @customers = Customer.all.page(params[:page]).per(10)
+  end
+
+  def show
+  end
+
+  def update
+  end
+
+  def edit
+  end
+end
