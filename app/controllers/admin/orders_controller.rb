9fd4e223e174230bd12c356ec4d0910768b455dd
DaisukeMaruyama <maruyama.d8@gmail.com>
2021-02-25 06:33:19 +0000

    [Add]管理者注文詳細追加

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -5,9 +5,29 @@ class Admin::OrdersController < ApplicationController
   end
 
   def show
+    @order = Order.find(params[:id])
+    @orders = Order.all
+    @shipping_cost = 800
+     # 商品合計(送料なし)
+    @payment_without_shipping = @order.total_payment -= @shipping_cost
+    # 商品合計(送料あり)
+    @payment_with_shipping = @order.total_payment += @shipping_cost
   end
 
   def update
+    @order = Order.find(params[:id])
+    if @order.update(admin_order_params)
+      flash[:notice] = "編集を保存しました。"
+      redirect_to admin_orders_path
+    else
+      render :show
+    end
+  end
+  
+  private
+  
+  def admin_order_params
+    params.require(:order).permit(:order_status)
   end
   
 end
--- app/models/order_detail.rb
+++ app/models/order_detail.rb
@@ -2,7 +2,9 @@ class OrderDetail < ApplicationRecord
 
   belongs_to :item
   belongs_to :order
-  
+
+  enum making_status: {制作不可:0,制作待ち:1, 制作中:2, 制作完了:3}
+
   def subtotal_price
     price * amount
   end
