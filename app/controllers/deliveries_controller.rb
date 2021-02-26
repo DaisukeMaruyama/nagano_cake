29b7f15f1d94b0104198ac3e4dd03caa8971003a
DaisukeMaruyama <maruyama.d8@gmail.com>
2021-02-26 04:13:20 +0000

    [Update]コントローラー編集

--- app/controllers/deliveries_controller.rb
+++ app/controllers/deliveries_controller.rb
@@ -1,4 +1,5 @@
 class DeliveriesController < ApplicationController
+  before_action :authenticate_customer!
 
   def index
     @delivery = Delivery.new
--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,5 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_customer!, except: [:index, :show]
 
 
   PER = 8
