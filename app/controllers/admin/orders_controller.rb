d628c6559c80294bce35bc65af405787bf2db80e
DaisukeMaruyama <maruyama.d8@gmail.com>
2021-02-28 07:29:31 +0000

    [Update]注文ステータス編集

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,6 +1,6 @@
 class Admin::OrdersController < ApplicationController
   before_action :authenticate_admin!
-  
+
   def index
     @orders = Order.all
   end
@@ -17,19 +17,24 @@ class Admin::OrdersController < ApplicationController
 
   def update
     @order = Order.find(params[:id])
-    if @order.update(admin_order_params)
+    if@order.update(admin_order_params)
+      if @order.order_status == "入金確認"
+        @order.order_details.each do |order_detail|
+      		order_detail.update(making_status: "制作待ち")
+        end
+		  end
       flash[:notice] = "編集を保存しました。"
       redirect_to admin_orders_path
     else
       render :show
     end
   end
-  
-  
+
+
   private
-  
+
   def admin_order_params
     params.require(:order).permit(:order_status)
   end
-  
+
 end
